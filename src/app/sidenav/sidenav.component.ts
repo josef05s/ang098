fa5f4dbe6a6a348847c9ee001520dec787aaac1d
Richard <richard.yanqui@upeu.edu.pe>
2023-09-11 11:06:20 -0500

    sidenav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+
 import { navbarData } from './nav-data';
 
+interface SideNavToggle{
+  screenWidth: number;
+  collapsed: boolean;
+}
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
@@ -8,7 +13,21 @@ import { navbarData } from './nav-data';
 })
 export class SidenavComponent {
 
-  collapsed = true;
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
+
+  collapsed = false;
+  screenWidth = 0;
   navData = navbarData;
+
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+
+  }
 
 }
